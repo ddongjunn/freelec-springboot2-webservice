e8efe30152deda736b95b8265c9617c34140fd3c
ddongjunn <ddongjunn@gmail.com>
2022-02-08 23:28:36 +0900

    네이버 로그인 구현, 기능 Test Code 완료

--- src/main/java/com/djlee/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/djlee/book/springboot/config/auth/SecurityConfig.java
@@ -13,22 +13,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션들을 disable
+                .headers().frameOptions().disable()
                 .and()
-                    .authorizeRequests() //URL별 권한 관리르 설정하는 옵션의 시작점, authorizeRequest가 선언되어야만 antMatchers 옵션을 사용할 수 있다.
-                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //권한 관리 대상을 지정하는 옵션
-                    .anyRequest().authenticated() //authenticated()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용 (로그인한 사용자)
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
                 .and()
                     .logout()
-                        .logoutSuccessUrl("/") //로그아웃 기능에 대한 여러 설정의 진입점, 로그아웃 성공 시 / 주소로 이동
+                        .logoutSuccessUrl("/")
                 .and()
-                    .oauth2Login() //OAuth2 로그인 기능에 대한 여러 설정의 진입점
-                        .userInfoEndpoint() //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
-                            .userService(customOAuth2UserService); //소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
-                                                                    //리소스 서버(소셜 서비스들)에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
     }
 }
